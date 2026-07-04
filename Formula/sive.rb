aa7a0bb16756c472aef9bc67b86e059bc1977782
David Åberg <git@davidaberg.me>
2026-07-04 14:07:24 +0200

    sive: v0.1.4 fix formula url+sha to published sdist asset

--- Formula/sive.rb
+++ Formula/sive.rb
@@ -3,7 +3,7 @@ class Sive < Formula
 
   desc "Sync secrets from your vault into your shell"
   homepage "https://github.com/PeachlifeAB/sive"
-  url "https://github.com/PeachlifeAB/sive/archive/refs/tags/0.1.4.tar.gz"
+  url "https://github.com/PeachlifeAB/sive/releases/download/v0.1.4/sive-0.1.4.tar.gz"
   sha256 "f99dc3928df4b2ff2ab64bb91845e8d0d2241fe3cd607ced0785f7866bcc5094"
   license "MIT"
 
