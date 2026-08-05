5db627079c98879c6d07ed5390dec9debb4ac9b8
David Åberg <git@davidaberg.me>
2026-08-05 23:21:36 +0200

    bgtail: 0.1.1

--- Formula/bgtail.rb
+++ Formula/bgtail.rb
@@ -1,20 +1,25 @@
 class Bgtail < Formula
   desc "Run long-running commands detached with minimal heartbeat"
   homepage "https://github.com/PeachlifeAB/bgtail"
-  url "https://github.com/PeachlifeAB/bgtail/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "8c93bca964e15df0ebc17a1fee1b54e5a5c74a578b2993fc79adc171a20e6912"
+  url "https://github.com/PeachlifeAB/bgtail/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "04e923bd2a1122d3d4e702dba826093006576e8c80f88937bd13958c194b2670"
   license "MIT"
+
+  bottle do
+    root_url "https://github.com/PeachlifeAB/homebrew-tap/releases/download/bgtail-0.1.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "33b64c9a2d4a55859d72c00c31660201f0159faca845c42e8ebbe5a779046077"
+  end
 
   depends_on "uv" => :build
   depends_on :macos
   depends_on "python@3.13"
 
   def install
-    python = Formula["python@3.13"].opt_bin/"python3.13"
+    python = formula_opt_bin("python@3.13")/"python3.13"
     system "uv", "pip", "install", "--no-deps", "--python", python, "--prefix", prefix, "."
   end
 
   test do
-    assert_match "0.1.0", shell_output("#{bin}/bgtail --version")
+    assert_match "0.1.1", shell_output("#{bin}/bgtail --version")
   end
 end
